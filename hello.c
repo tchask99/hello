0abed865fcfe54a7422c3f66f34da175a934f573
tchask99 <46799567+tchask99@users.noreply.github.com>
2019-01-24 08:43:12 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,7 +3,8 @@
 int main (void){
 
 // Hack 1 created by Tanner Haskins for CSCE 155E Section 150
-
+// Edited by
+  
 printf("Hello, my name is Tanner Haskins\n");
 printf("My major is Electrical Engineering\n");
 
